584e14d1b1ff3c2f7e9f65f15415da696f49c1df
apulchritudoff <32424649+apulchritudoff@users.noreply.github.com>
2021-12-02 11:06:25 -0800

    First Commit

---
+++ techtalentblog/src/main/java/com/techtalentsouth/techtalentblog/BlogPost/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.techtalentsouth.techtalentblog.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
+
+}
